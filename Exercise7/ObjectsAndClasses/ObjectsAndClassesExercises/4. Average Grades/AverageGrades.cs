4bd2c3771a53ecb88f27ee35cf13b74ce093333d
Nadejda <nadezhda.tsvetanova@gmial.com>
2018-02-24 20:55:49 +0200

    Change123

--- Exercise7/ObjectsAndClasses/ObjectsAndClassesExercises/4. Average Grades/AverageGrades.cs
+++ Exercise7/ObjectsAndClasses/ObjectsAndClassesExercises/4. Average Grades/AverageGrades.cs
@@ -50,21 +50,5 @@ namespace _4.Average_Grades
             }     
            
         }
-        //class Students
-        //{
-        //    public string Name { get; set; }
-        //    public List<double> Grades { get; set; }
-        //    public double AverageGrade { get; set; }
-
-        //    public double Average(List<double> Grades)
-        //    {
-        //        var sum = 0d;
-        //        for (int i = 0; i < Grades.Count; i++)
-        //        {
-        //            sum += Grades[i];
-        //        }
-        //        return sum / Grades.Count;
-        //    }
-        //}
     }
 }
---
+++ Exercise7/ObjectsAndClasses/ObjectsAndClassesExercises/9.TeamworkProjects/TeamworkProjects.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _9.TeamworkProjects
+{
+    class TeamworkProjects
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
